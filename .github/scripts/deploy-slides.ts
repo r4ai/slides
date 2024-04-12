87a0eec77bcd84c69c5cd42c04849a39ffb6b5c6
rai <mj82xx@gmail.com>
2024-04-12 10:11:26 +0900

    chore: deploy with github pages

--- .github/scripts/deploy-slides.ts
+++ .github/scripts/deploy-slides.ts
@@ -1,11 +1,22 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 import { $ } from "bun";
 
-const rootDir = await $`git rev-parse --show-toplevel`.text();
+const rootDir = (await $`git rev-parse --show-toplevel`.text()).trim();
+
 const distDir = path.join(rootDir, "dist");
+if (await fs.exists(distDir)) {
+  await fs.rm(distDir, { recursive: true });
+}
+await fs.mkdir(distDir);
+
 const slidesDir = path.join(rootDir, "slides");
 
-for (const dir of fs.readdirSync(slidesDir)) {
-  const slideDistDir = path.join(slidesDir, dir, "dist");
+// cp slides/${slideDir}/dist dist/${slideDir}
+for (const slideDir of await fs.readdir(slidesDir)) {
+  const slideDistDir = path.join(slidesDir, slideDir, "dist");
+  if (!fs.exists(slideDistDir)) {
+    continue;
+  }
+  await fs.cp(slideDistDir, path.join(distDir, slideDir), { recursive: true });
 }
